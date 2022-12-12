5c58fef257dc26b943dfe5efd4a74e5396c9dfb2
matteobnvl <matteobonneval19@gmail.com>
2022-12-12 17:46:37 +0100

    page about

--- assets/js/app.js
+++ assets/js/app.js
@@ -133,6 +133,34 @@ function handleMouseLeave(e) {
     });
 }
 
+const btn2019 = document.getElementById('btn-2019')
+const btn2021 = document.getElementById('btn-2021')
+const btn2024 = document.getElementById('btn-2024')
+
+const box2019 = document.getElementById('box-2019')
+const box2021 = document.getElementById('box-2021')
+const box2024 = document.getElementById('box-2024')
+console.log(document.getElementById('box-2019'))
+
+btn2019.addEventListener('click', function(){
+    box2019.classList.add('active')
+    box2021.classList.remove('active')
+    box2024.classList.remove('active')
+})
+
+btn2021.addEventListener('click', function(){
+    box2021.classList.add('active')
+    box2019.classList.remove('active')
+    box2024.classList.remove('active')
+})
+
+btn2024.addEventListener('click', function(){
+    box2024.classList.add('active')
+    box2021.classList.remove('active')
+    box2019.classList.remove('active')
+})
+
+
 
 const linkA = document.getElementsByTagName('a')
 const cursor = document.getElementById('cursor')
@@ -144,5 +172,3 @@ for(const element in linkA) {
         cursor.classList.remove('exclude-cursor')
     })
 };
-
-linkA.mouse
